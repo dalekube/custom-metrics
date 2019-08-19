89f3499e1d70cf68bc6fac1a602ede590411a832
Dale <dkube@uwalumni.com>
2019-08-18 19:59:05 -0500

    F1 score

---
+++ F1-score-LightGBM.R
@@ -0,0 +1,25 @@
+
+library(Laurae)
+
+# Custom F1 score evaluation metric for LightGBM
+# Use the 'get.max_f1' function from the Laurae package
+F1_metric = function(preds,dtrain){
+  
+  labels = getinfo(dtrain,"label")
+  F1 = get.max_f1(preds,labels)[1]
+  return(list(name="F1",value=F1,higher_better=TRUE))
+  
+}
+
+# # Fit the model
+# lgb.model = lgb.cv(
+#   params=params,
+#   data=dtrain,
+#   nfold=5,
+#   stratified=T,
+#   nrounds=2500,
+#   num_threads=8,
+#   early_stopping_rounds=100,
+#   eval=F1_metric,
+#   verbose=1
+# )
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # custom-metrics
 Custom metrics for LightGBM in R
+
+This repository represents a collection of custom metrics for use with LightGBM implementations in R.
